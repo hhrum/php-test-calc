185357ecb1f95db3ef325a308eb31e0b0a9109f1
hhrum <denis.smit72@gmail.com>
2020-12-07 02:29:36 +0500

    Добавил DatePicker и слайдеры

--- js/script.js
+++ js/script.js
@@ -1,31 +1,54 @@
 // Константные значения
-const depositeAmountRange = [1000, 30000];
-const depositReplenishmentAmounttRange = [1000, 30000];
+const depositeAmountRange = [1000, 3000000];
+const depositReplenishmentAmounttRange = [1000, 3000000];
 
 var calculatorForm = $("#calculatorForm");
+calculatorForm.find("#datepicker").datepicker();
 
-calculatorForm.submit(function(e) {
-    e.preventDefault(); 
+$(function () {
+    makeSliderForNumericInput("#depositeAmount", depositeAmountRange);
+    makeSliderForNumericInput("#depositReplenishmentAmount", depositReplenishmentAmounttRange);
 
-    if (!formIsNotEmpty(calculatorForm)) {
-        alert("Не все поля заполнены!");
-        return
-    }
+    calculatorForm.submit(function (e) {
+        e.preventDefault();
 
-    var url = calculatorForm.attr('action');
+        if (!formIsNotEmpty(calculatorForm)) {
+            alert("Не все поля заполнены!");
+            return
+        }
 
-    $.ajax({
-           type: "POST",
-           url: url,
-           data: calculatorForm.serialize(), 
-           success: function(data)
-           {
-               alert(data); // show response from the php script.
-           }
-         });
+        var url = calculatorForm.attr('action');
 
-    
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: calculatorForm.serialize(),
+            success: function (data) {
+                alert(data); // show response from the php script.
+            }
+        });
+
+    });
 });
+
+function makeSliderForNumericInput(inputId, range) {
+    var depositeAmountInput = $(inputId);
+    depositeAmountInput.attr('min', range[0]);
+    depositeAmountInput.val(range[0]);
+
+    var slider = $("<div class='calculator__slider'></div>").insertAfter(depositeAmountInput).slider({
+        min: range[0],
+        max: range[1],
+        range: "min",
+        value: depositeAmountInput.val(),
+        slide: function (event, ui) {
+            depositeAmountInput.val(ui.value);
+        }
+    });
+    depositeAmountInput.on("change", function () {
+        slider.slider("value", $(this).val());
+    });
+}
 
 function formIsNotEmpty(form) {
     var inputs = form.serializeArray();
